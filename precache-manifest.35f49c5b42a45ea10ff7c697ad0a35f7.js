10608476acaa28018809158f13de02fc0cd6a9d8
EXTERNAL Fang Chenguang (Golden Future <fac2szh@bosch.com>
2020-11-26 10:06:15 +0800

    build

---
+++ precache-manifest.35f49c5b42a45ea10ff7c697ad0a35f7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ea6c6b72502b4fb111",
+    "url": "css/app.2a31c668.css"
+  },
+  {
+    "revision": "0996fa7bdc2f87bef1eb",
+    "url": "css/chunk-vendors.8de8c62e.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "c5ef48269c0af1cfaea134e39f29bfae",
+    "url": "img/timg.c5ef4826.png"
+  },
+  {
+    "revision": "53a8b4063ef03891abaab9e1faf8b6ab",
+    "url": "index.html"
+  },
+  {
+    "revision": "c9ea6c6b72502b4fb111",
+    "url": "js/app.ebdba96b.js"
+  },
+  {
+    "revision": "0996fa7bdc2f87bef1eb",
+    "url": "js/chunk-vendors.78a47d7c.js"
+  },
+  {
+    "revision": "e0680387026428a3fa17d744c644e1b5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "c5ef48269c0af1cfaea134e39f29bfae",
+    "url": "timg.png"
+  }
+]);
